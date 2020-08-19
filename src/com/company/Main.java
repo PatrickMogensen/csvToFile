77c925614207df615e49206c7b859bcf07ef16ee
PatrickMogensen <57892602+PatrickMogensen@users.noreply.github.com>
2020-08-20 00:50:09 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -10,20 +12,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException, NullPointerException {
        URL url = new URL("https://drive.google.com/u/0/uc?id=1bj8hvAteDn-yKUPFasBSvg9P8jvripwd&export=download");
-        //  InputStream in = url.openStream();
 
-        // Scanner scan = new Scanner(in);
+        File file = new File("fourColumns.csv");
 
-       csvReader csvr = new csvReader(url);
+        // creates the file
+        file.createNewFile();
 
-      List<LineObj> list = csvr.scanCSV();
+        // creates a FileWriter Object
+        FileWriter writer = new FileWriter(file);
 
-      //  System.out.println(list.get(1));
+         csvReader csvr = new csvReader(url);
+
+         List<LineObj> list = csvr.scanCSV();
+
+        String[] firstLine = csvr.getFirstLine(url);
+
+        writer.write(firstLine[1] + "," + firstLine[11] + "," + firstLine[10] + "," + firstLine[23] + " \n");
+
         for (int i = 0; i < list.size() ; i++) {
-            System.out.print(list.get(i).getActor_1_name() + " ");
-            System.out.print(list.get(i).getDirector_name() + " ");
-            System.out.print(list.get(i).getMovie_title() + " ");
-            System.out.print(list.get(i).getTitle_year() + " \n");
+
+            writer.write(list.get(i).getDirector_name() + ",");
+            writer.write(list.get(i).getMovie_title() + ",");
+            writer.write(list.get(i).getActor_1_name() + ",");
+            writer.write(list.get(i).getTitle_year() + " \n");
 
 
 
